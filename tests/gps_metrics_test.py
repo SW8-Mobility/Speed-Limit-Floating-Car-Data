f3e6995c81d2f8c9bf33ba5ac806005443443321
Alexander Nesheim <alexander.nesheim@gmail.com>
2023-03-20 13:28:32 +0100

    Added simple test (and fixed wrong euclidean dist calculation in source)

---
+++ tests/gps_metrics_test.py
@@ -0,0 +1,15 @@
+from geo_json_metrics.gps_metrics import calc_utm_dist
+
+
+def test_calc_utm_dist() -> bool:
+    # Arrange
+    coordinate = tuple[float, float] # type alias
+    cord1: coordinate = (2, 3)
+    cord2: coordinate = (5, 3)
+    expected_utm_dist: float = 3
+
+    # Act
+    actual_utm_dist: float = calc_utm_dist(cord1, cord2)
+
+    # Assert
+    assert expected_utm_dist == actual_utm_dist
