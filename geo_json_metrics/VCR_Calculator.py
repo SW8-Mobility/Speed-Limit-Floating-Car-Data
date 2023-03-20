d65768be0909072cdb08ff02fff80f70342aa4e6
Alexander Nesheim <alexander.nesheim@gmail.com>
2023-03-20 09:55:43 +0100

    Added docstrings (now Google-formatted!!)

--- geo_json_metrics/VCR_Calculator.py
+++ geo_json_metrics/VCR_Calculator.py
@@ -1,15 +1,22 @@
 def map_vcr(velocities: list[float]) -> list[float]:
+    """Map the VCR function over each pair from the list, skipping the first element
+    Args:
+        velocities (list[float]): A list of floats corresponding to the velocities/speeds
+
+    Returns:
+        list[float]: new list of Velocity Change Rates (the list is length of inputlist - 1)
+    """
     return list(map(lambda v1, v2: vcr(v1, v2), velocities[:-1], velocities[1:]))
 
 
 def vcr(v1: float, v2: float) -> float:
-    """
-    :param v1: Velocity 1
-    :type v1: float
-    :param v2: Velocity 2
-    :type v2: float
-    :return: The Velocity Change Rate between the input velocities
-    :rtype: float
+    """Calculate Velocity Change Rate between two velocities
+    Args:
+        v1 (float): Velocity 1
+        v2 (float): Velocity 2
+
+    Returns:
+        float: Velocity Change Rate between the two inputs
     """
     return (v2 - v1)/v2
 
