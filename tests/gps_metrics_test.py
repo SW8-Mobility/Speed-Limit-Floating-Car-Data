7eff84ef7fe2395bb650b115657e28955dcc5eb1
Lint Action <lint-action@samuelmeuli.com>
2023-03-23 13:15:24 +0000

    Fix code style issues with Black

--- tests/gps_metrics_test.py
+++ tests/gps_metrics_test.py
@@ -4,7 +4,12 @@ from typing import Union
 import numpy as np
 import pandas as pd
 import pytest
-from geo_json_metrics.gps_metrics import shift_elems, calc_utm_dist, create_df_from_json, calculate_distance_and_speed
+from geo_json_metrics.gps_metrics import (
+    shift_elems,
+    calc_utm_dist,
+    create_df_from_json,
+    calculate_distance_and_speed,
+)
 
 
 @pytest.mark.parametrize(
@@ -34,15 +39,14 @@ def test_shift_elems(test_input: list, expected: list) -> None:
 def test_calc_utm_dist(utm1, utm2, expected):
     assert calc_utm_dist(utm1, utm2) == expected
 
+
 def test_create_df_from_json():
     expected_data = {
         "id": "test_id",
         "length": 1234,
         "end_date": "test_date",
         "start_date": "test_date",
-        "osm_id": [
-            [123456789, 123456789, 123456789]
-        ],
+        "osm_id": [[123456789, 123456789, 123456789]],
         "coordinates": [
             [
                 [100.0, 100.0, 1000000000.0],
@@ -56,15 +60,14 @@ def test_create_df_from_json():
     actual_df = create_df_from_json(testfile_path)
     assert expected_df.equals(actual_df)
 
+
 def test_calculate_distance_and_speed():
     expected_data = {
         "id": "test_id",
         "length": 1234,
         "end_date": "test_date",
         "start_date": "test_date",
-        "osm_id": [
-            [123456789, 123456789, 123456789]
-        ],
+        "osm_id": [[123456789, 123456789, 123456789]],
         "coordinates": [
             [
                 [100.0, 100.0, 1000000000.0],
@@ -72,16 +75,8 @@ def test_calculate_distance_and_speed():
                 [100.0, 130.0, 1000000002.0],
             ]
         ],
-        "distances": [
-            [
-                10.0, 20.0
-            ]
-        ],
-        "speeds": [
-            [
-                36.0, 72.0
-            ]
-        ]
+        "distances": [[10.0, 20.0]],
+        "speeds": [[36.0, 72.0]],
     }
     expected_df = pd.DataFrame(data=expected_data)
 
@@ -90,20 +85,19 @@ def test_calculate_distance_and_speed():
         "length": 1234,
         "end_date": "test_date",
         "start_date": "test_date",
-        "osm_id": [
-            [123456789, 123456789, 123456789]
-        ],
+        "osm_id": [[123456789, 123456789, 123456789]],
         "coordinates": [
             [
                 [100.0, 100.0, 1000000000.0],
                 [100.0, 110.0, 1000000001.0],
                 [100.0, 130.0, 1000000002.0],
             ]
-        ]
+        ],
     }
     actual_df = pd.DataFrame(data=actual_data)
     calculate_distance_and_speed(actual_df)
     assert actual_df.speeds.equals(expected_df.speeds)
+
 
 def test_calculate_distance_and_speed2():
     expected_data = {
@@ -111,20 +105,14 @@ def test_calculate_distance_and_speed2():
         "length": 1234,
         "end_date": "test_date",
         "start_date": "test_date",
-        "osm_id": [
-            [123456789, 123456789, 123456789]
-        ],
+        "osm_id": [[123456789, 123456789, 123456789]],
         "coordinates": [
             [
                 [100.0, 100.0, 1000000000.0],
             ]
         ],
-        "distances": [
-            []
-        ],
-        "speeds": [
-            []
-        ]
+        "distances": [[]],
+        "speeds": [[]],
     }
     expected_df = pd.DataFrame(data=expected_data)
 
@@ -133,14 +121,12 @@ def test_calculate_distance_and_speed2():
         "length": 1234,
         "end_date": "test_date",
         "start_date": "test_date",
-        "osm_id": [
-            [123456789, 123456789, 123456789]
-        ],
+        "osm_id": [[123456789, 123456789, 123456789]],
         "coordinates": [
             [
                 [100.0, 100.0, 1000000000.0],
             ]
-        ]
+        ],
     }
     actual_df = pd.DataFrame(data=actual_data)
     calculate_distance_and_speed(actual_df)
@@ -155,7 +141,7 @@ def test_calculate_distance_and_speed3():
         "start_date": "test_date",
         "osm_id": [
             [123456789, 123456789, 123456789],
-            [123456789, 123456789, 123456789]
+            [123456789, 123456789, 123456789],
         ],
         "coordinates": [
             [
@@ -167,16 +153,10 @@ def test_calculate_distance_and_speed3():
                 [100.0, 100.0, 1000000000.0],
                 [150.0, 100.0, 1000000001.0],
                 [200.0, 100.0, 1000000002.0],
-            ]
+            ],
         ],
-        "distances": [
-            [10.0, 20.0],
-            [50.0, 50.0]
-        ],
-        "speeds": [
-            [36.0, 72.0],
-            [180.0, 180.0]
-        ]
+        "distances": [[10.0, 20.0], [50.0, 50.0]],
+        "speeds": [[36.0, 72.0], [180.0, 180.0]],
     }
     expected_df = pd.DataFrame(data=expected_data)
 
@@ -187,7 +167,7 @@ def test_calculate_distance_and_speed3():
         "start_date": "test_date",
         "osm_id": [
             [123456789, 123456789, 123456789],
-            [123456789, 123456789, 123456789]
+            [123456789, 123456789, 123456789],
         ],
         "coordinates": [
             [
@@ -199,8 +179,8 @@ def test_calculate_distance_and_speed3():
                 [100.0, 100.0, 1000000000.0],
                 [150.0, 100.0, 1000000001.0],
                 [200.0, 100.0, 1000000002.0],
-            ]
-        ]
+            ],
+        ],
     }
     actual_df = pd.DataFrame(data=actual_data)
     calculate_distance_and_speed(actual_df)
