9ee8f3317861fb85284d692da12d13c054ebca64
AlexanderHHansen <61501072+AlexanderHHansen@users.noreply.github.com>
2023-04-05 10:00:36 +0200

    example of file, that now works, without sys.path hack

--- archive/analysis_metrics.py
+++ archive/analysis_metrics.py
@@ -3,8 +3,6 @@
 
 import sys, os
 
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
-
 from functools import reduce
 import json
 from math import sqrt
@@ -131,9 +129,9 @@ def main():
     filename = "C:/Users/ax111/Documents/Personal documents/Coding/SW8/speed_limit_floating_car_data/archive/segment_10240935_linestring.json"
     df = create_df_from_json(filename)
     university_boulevard_osm_id = 10240935
-    filtered_df = filter_segments(df, university_boulevard_osm_id)
-    calculate_distance_and_speed(filtered_df)
-    avg, min, max = calculate_metrics(filtered_df)
+    # filtered_df = filter_segments(df, university_boulevard_osm_id)
+    calculate_distance_and_speed(df)
+    avg, min, max = calculate_metrics(df)
     print(avg, min, max, sep=", ")  # keep
 
 
