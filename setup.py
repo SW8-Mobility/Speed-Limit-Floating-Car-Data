b97c85d600f838cb82abc854a9f3cb57074de62c
Lint Action <lint-action@samuelmeuli.com>
2023-04-05 08:16:56 +0000

    Fix code style issues with Black

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='speed_limit_floating_car_data', version='1.0', packages=find_packages())
+setup(name="speed_limit_floating_car_data", version="1.0", packages=find_packages())
