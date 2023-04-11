3775570ad2ca5367d32fca87b2a1c6e6b1fe7e77
frejaz <fhpe19@student.aau.dk>
2023-04-11 21:14:59 +0200

    added function and fixed test, however, there is a small issue with the path

--- pipeline/preprocessing/process_merged_geojson.py
+++ pipeline/preprocessing/process_merged_geojson.py
@@ -2,29 +2,32 @@ import json
 import pandas as pd
 
 def create_df_from_merged_osm_vejman(filename: str) -> pd.DataFrame:
+    """
+    Create a dataframe from merged between osm and vejman data
+    Args:
+        filename: a file in geojson format
 
-    with open(filename, "r") as json_file:
+    Returns: dataframe with formated columns
+
+    """
+
+    with open(filename, "r", encoding='utf-8') as json_file:
         data = json.load(json_file)
 
     df = pd.DataFrame.from_records(  # Convert json file to dataframe
         data["features"] # Ignore other headers
     )
 
-    # Unnest some of the nested values - TODO: this could easily be a function
-    df["osm_id"] = df["properties"].apply(lambda prop_dict: prop_dict["osm_id"])
-    df["vm_vejnavn"] = df["properties"].apply(lambda prop_dict: prop_dict["CPR_VEJNAVN"])
-    df["hast_general_hast"] = df["properties"].apply(lambda prop_dict: prop_dict["HAST_GENEREL_HAST"])
-    df["kode_hast_generel_hast"] = df["properties"].apply(lambda prop_dict: prop_dict["KODE_HAST_GENEREL_HAST"])
-    df["hast_gaeldende_hast"] = df["properties"].apply(lambda prop_dict: prop_dict["HAST_GAELDENDE_HAST"])
-    df["vejstiklasse"] = df["properties"].apply(lambda prop_dict: prop_dict["VEJSTIKLASSE"])
-    df["kode_vejstiklasse"] = df["properties"].apply(lambda prop_dict: prop_dict["KODE_VEJSTIKLASSE"])
-    df["vejtypeskiltet"] = df["properties"].apply(lambda prop_dict: prop_dict["VEJTYPESKILTET"])
-    df["kode_vejtypeskiltet"] = df["properties"].apply(lambda prop_dict: prop_dict["KODE_VEJTYPESKILTET"])
-    df["hast_senest_rettet"] = df["properties"].apply(lambda prop_dict: prop_dict["HAST_SENEST_RETTET"])
+    # Unnest some nested values
+    property_list = ["osm_id", "CPR_VEJNAVN", "HAST_GENEREL_HAST", "KODE_HAST_GENEREL_HAST", "HAST_GAELDENDE_HAST",
+                   "VEJSTIKLASSE", "KODE_VEJSTIKLASSE", "VEJTYPESKILTET", "KODE_VEJTYPESKILTET", "HAST_SENEST_RETTET"]
+    unnest_df(df, "properties", property_list)
 
-    df["coordinates"] = df["geometry"].apply(lambda prop_dict: prop_dict["coordinates"])
+    geometry_list = ["coordinates"]
+    unnest_df(df, "geometry", geometry_list)
 
-    df.drop(  # drop unused columns
+    # drop unused columns
+    df.drop(
         ["type", "properties", "geometry"], inplace=True, axis=1
     )
 
@@ -32,9 +35,21 @@ def create_df_from_merged_osm_vejman(filename: str) -> pd.DataFrame:
 
     return df
 
+def unnest_df(df: pd.DataFrame, nest_header: str, key_list: list[str]) -> None:
+    """
+    Unnest a given key from a column, if the format is like a dictionary
+    Args:
+        df: a dataframe
+        nest_header: the column where the nested values are
+        key_list: a list of keys to look for
+    """
+    for key in key_list:
+        df[key.lower()] = df[nest_header].apply(lambda dict: dict[key])
+
 def main():
-    filename = "C:/Users/freja/Desktop/Speed-Limit-Floating-Car-Data/tests/test_files/merged_extract_v1.json"
+    filename = "C:/Users/freja/Desktop/Speed-Limit-Floating-Car-Data/tests/test_files/vejman_osm_merge_extract_data.json"
     df = create_df_from_merged_osm_vejman(filename)
+    df.to_csv("from_fun.csv")
 
 if __name__ == "__main__":
     main()
