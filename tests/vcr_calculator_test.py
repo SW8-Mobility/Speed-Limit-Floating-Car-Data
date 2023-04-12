749d0f1bd289de4146680ce9eea514052612c1a2
AlexanderHHansen <61501072+AlexanderHHansen@users.noreply.github.com>
2023-04-12 10:39:18 +0200

    removed evals (#44)
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>
    Co-authored-by: frejaz <56343992+frejaz@users.noreply.github.com>
    Co-authored-by: frejaz <fhpe19@student.aau.dk>

--- tests/vcr_calculator_test.py
+++ tests/vcr_calculator_test.py
@@ -11,7 +11,7 @@ from pipeline.preprocessing.compute_features.vcr_calculator import (
     "test_input, expected_vcrs",
     [
         (
-            f"{map_vcr([1,2,3,4,5,6,7])}",
+            [1, 2, 3, 4, 5, 6, 7],
             [
                 0.5,
                 0.3333333333333333,
@@ -21,19 +21,19 @@ from pipeline.preprocessing.compute_features.vcr_calculator import (
                 0.14285714285714285,
             ],
         ),
-        (f"{map_vcr([3, 2])}", [-0.5]),
+        ([3, 2], [-0.5]),
     ],
 )
 def test_map_vcr(test_input, expected_vcrs: list[float]) -> None:
-    assert eval(test_input) == expected_vcrs
+    assert map_vcr(test_input) == expected_vcrs
 
 
 @pytest.mark.parametrize(
-    "test_input, expected_vcr",
-    [(f"{vcr(55.78, 45.0)}", -0.23955555555555558), (f"{vcr(40, 50)}", 0.2)],
+    "v1, v2, expected_vcr",
+    [(55.78, 45.0, -0.23955555555555558), (40, 50, 0.2)],
 )
-def test_vcr(test_input, expected_vcr) -> None:
-    assert eval(test_input) == expected_vcr
+def test_vcr(v1, v2, expected_vcr) -> None:
+    assert vcr(v1, v2) == expected_vcr
 
 
 @pytest.mark.parametrize(
