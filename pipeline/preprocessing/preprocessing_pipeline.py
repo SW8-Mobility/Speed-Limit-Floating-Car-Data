00589414d8441b6719f079973317587983820a72
Lint Action <lint-action@samuelmeuli.com>
2023-04-14 08:00:38 +0000

    Fix code style issues with Black

--- pipeline/preprocessing/preprocessing_pipeline.py
+++ pipeline/preprocessing/preprocessing_pipeline.py
@@ -1,6 +1,9 @@
 from pipeline.preprocessing.format_data import create_segment_to_coordinate_df
-from pipeline.preprocessing.process_merged_geojson import create_df_from_merged_osm_vejman
+from pipeline.preprocessing.process_merged_geojson import (
+    create_df_from_merged_osm_vejman,
+)
 import pandas as pd
+
 
 def main():
     # Use ground truth
@@ -12,6 +15,7 @@ def main():
     df = pd.read_pickle(...).infer_objects()
     mapped_df = create_segment_to_coordinate_df(df)
     mapped_df.to_pickle("raw_data.pkl")
+
 
 if __name__ == "__main__":
     main()
