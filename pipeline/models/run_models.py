f83d096b41dfdc79750cf6bee2d81481c63655c3
Alexander Nesheim <alexander.nesheim@gmail.com>
2023-04-17 12:00:11 +0200

    train test split added.

--- pipeline/models/run_models.py
+++ pipeline/models/run_models.py
@@ -1,4 +1,5 @@
 from pipeline.preprocessing.compute_features.feature import Feature
+from sklearn.model_selection import train_test_split
 import pandas as pd
 pd.options.display.width = 0
 
@@ -13,15 +14,21 @@ def get_fake_input():
                                    [[150, 2000, 100], [3000, 400, 150], [5000, 430, 4600]], [26.67, 40.0, 46.33], 37.67,
                                    [10, 30, 43], 10, [40, 50, 43], 50, [[20, 35], [35, 45], [46.5, 44.5]], [30, 40, 46],
                                    40]], columns=columns)
+
     return input_df
 
 
 def run_models():
     input_df = get_fake_input()
+
     data = [[123, "testvej", "testvej_osm", 123]]
     columns = ['osm_id', 'cpr_name', 'osm_name', 'id']
-    df_non_features = pd.DataFrame(data=data, columns=columns)
+    output_df = pd.DataFrame(data=data, columns=columns)
 
+    X = input_df.drop(columns=['target'])
+    y = input_df['target']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
 def main():
