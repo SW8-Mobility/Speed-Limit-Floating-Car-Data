622fae165469189a96f7cf29a60a67379430fbe2
AlexanderHHansen <61501072+AlexanderHHansen@users.noreply.github.com>
2023-04-18 08:24:42 +0200

    moved to geojson.py

--- pipeline/preprocessing/ground_truth_processing/open_street/osm_geometry.py
+++ pipeline/preprocessing/ground_truth_processing/open_street/osm_geometry.py
@@ -84,31 +84,7 @@ def get_osmid_to_linestring_dictionary(
     geometryHandler = GeometryRoadHandler()
     geometryHandler.apply_file(OSMFilePath, locations=True)
 
-    return geometryHandler.geometryDictionary
-
-
-def annotate_geojson_with_speedlimit(
-    geojson: dict, df_with_speedlimit: pd.DataFrame
-) -> None:
-    """Annotates a geojson dict with predicted speedlimits from a dataframe.
-    If there is no predicted value for a segment, the default will be 'na'.
-
-    Args:
-        geojson (dict): geojson as a dictionary
-        df_with_speedlimit (pd.DataFrame): dataframe with speedlimits
-    """
-    # index using osm_id
-    df_with_speedlimit["index"] = df_with_speedlimit[Feature.OSM_ID.value]
-    df_with_speedlimit = df_with_speedlimit.set_index("index")
-
-    for segment in geojson["features"]:
-        osm_id = segment["properties"]["osm_id"]
-        try:
-            segment["properties"][Feature.SPEED_LIMIT.value] = df_with_speedlimit.loc[
-                osm_id
-            ][Feature.SPEED_LIMIT.value]
-        except:  # no predicted value for osm_id
-            segment["properties"][Feature.SPEED_LIMIT.value] = "na"
+    return geometryHandler.geometryDictionary   
 
 
 def main() -> None:
