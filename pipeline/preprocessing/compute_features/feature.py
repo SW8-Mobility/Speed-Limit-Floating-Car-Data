68eb68f3902cc4cf0548cd6d7f5864de1c2010b3
AlexanderHHansen <61501072+AlexanderHHansen@users.noreply.github.com>
2023-04-19 10:38:14 +0200

    vejman features

--- pipeline/preprocessing/compute_features/feature.py
+++ pipeline/preprocessing/compute_features/feature.py
@@ -21,7 +21,16 @@ class Feature(Enum):
     VCR = "vcr"
     DAY_OF_WEEK = "day_of_week"
     TIME_GROUP = "time_group"
-    # more to come
+    SPEED_LIMIT_TARGET = "speed_limit_target"
+    SPEED_LIMIT_PREDICTED = "speed_limit_predicted"
+
+    # vejman features 
+    CPR_VEJNAVN = 'cpr_vejnavn'
+    HAST_GENEREL_HAST = 'hast_generel_hast' 
+    HAST_GAELDENDE_HAST = 'hast_gaeldende_hast'
+    VEJSTIKLASSE = 'vejstiklasse'
+    VEJTYPESKILTET = 'vejtypeskiltet'
+    HAST_SENEST_RETTET = "hast_senest_rettet"
 
     def __str__(self) -> str:
         return self.value
