660ab38a8dd892de3536c26dee77367af0efa9e3
AlexanderHHansen <61501072+AlexanderHHansen@users.noreply.github.com>
2023-05-01 10:19:06 +0200

    more feature tests

--- tests/feature_test.py
+++ tests/feature_test.py
@@ -25,6 +25,40 @@ def test_feature_eq(feature1, feature2, expected):
     assert actual == expected
 
 @pytest.mark.parametrize(
+    "feature_list1, feature_list2, expected",
+    [
+        (
+            FeatureList([Feature.SPEEDS]),
+            FeatureList([Feature.SPEEDS]),
+            True
+        ),
+        (
+            FeatureList([Feature.SPEEDS]),
+            FeatureList([Feature.DISTANCES]),
+            False
+        ),
+        (
+            FeatureList([]),
+            FeatureList([]),
+            True
+        ),
+        (
+            FeatureList([Feature.DISTANCES, Feature.SPEEDS]),
+            FeatureList([Feature.DISTANCES]),
+            False
+        ),
+        (
+            FeatureList([Feature.DISTANCES, Feature.SPEEDS]),
+            FeatureList([]),
+            False
+        ),
+    ],
+)
+def test_feature_list_eq(feature_list1, feature_list2, expected):
+    actual = feature_list1 == feature_list2
+    assert actual == expected
+
+@pytest.mark.parametrize(
     "input_list1, input_list2, expected",
     [
         (
@@ -75,6 +109,35 @@ def test_feature_list_minus(input_list1, input_list2, expected_list):
     fl1 = FeatureList(input_list1)
     fl2 = FeatureList(input_list2)
     actual = fl1 - fl2 
+    assert actual == expected
+
+@pytest.mark.parametrize(
+    "feature_list1, feature_list2, expected",
+    [
+        (
+            FeatureList([Feature.SPEEDS]),
+            FeatureList([Feature.SPEEDS]),
+            FeatureList([Feature.SPEEDS, Feature.SPEEDS])
+        ),
+        (
+            FeatureList([Feature.SPEEDS]),
+            FeatureList([Feature.DISTANCES]),
+            FeatureList([Feature.SPEEDS, Feature.DISTANCES])
+        ),
+        (
+            FeatureList([Feature.SPEEDS, Feature.DISTANCES]),
+            FeatureList([]),
+            FeatureList([Feature.SPEEDS, Feature.DISTANCES])
+        ),
+        (
+            FeatureList([]),
+            FeatureList([Feature.SPEEDS, Feature.DISTANCES]),
+            FeatureList([Feature.SPEEDS, Feature.DISTANCES])
+        ),
+    ],
+)
+def test_feature_list_add(feature_list1, feature_list2, expected):
+    actual = feature_list1 + feature_list2
     assert actual == expected
 
 @pytest.mark.parametrize(
