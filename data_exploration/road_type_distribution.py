4584cfde8645fb9256d5e2b1134a7484aeaaa9a1
Lint Action <lint-action@samuelmeuli.com>
2023-05-13 10:11:50 +0000

    Fix code style issues with Black

--- data_exploration/road_type_distribution.py
+++ data_exploration/road_type_distribution.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from pipeline.preprocessing.compute_features.feature import Feature
 
-share_files = '/share-files/pickle_files_features_and_ground_truth/'
+share_files = "/share-files/pickle_files_features_and_ground_truth/"
 
 paths = [
-    share_files+'2012.pkl',
-    share_files+'2013.pkl',
-    share_files+'2014.pkl',
+    share_files + "2012.pkl",
+    share_files + "2013.pkl",
+    share_files + "2014.pkl",
 ]
 
 for p in paths:
