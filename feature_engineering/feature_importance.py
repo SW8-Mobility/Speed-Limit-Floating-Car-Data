4073992109966d30040f63e6b3aef3f29eac1e10
kubbe <ahha19@student.aau.dk>
2023-05-14 08:54:45 +0000

    changes

--- feature_engineering/feature_importance.py
+++ feature_engineering/feature_importance.py
@@ -1,6 +1,7 @@
 import joblib
 from sklearn.inspection import permutation_importance
 from pipeline.preprocessing.sk_formatter import SKFormatter
+import glob
 
 # ------------------------EXAMPLE-----------------------------
 from sklearn.model_selection import train_test_split
@@ -57,13 +58,15 @@ def calculate_feature_importance(model, X_val, y_val) -> None:
                 )
 
 def main():
-    formatter = SKFormatter(dataset="/share-files/pickle_files_features_and_ground_truth/2012.pkl", dataset_size=1000, test_size=0)
+    model_folder = "/share-files/share-files/runs/05_12_12_18/"
+    paths = glob.glob(model_folder + '*.joblib')
+
+    formatter = SKFormatter(dataset="/share-files/share-files/raw_data_pkl/features_and_ground_truth_2012.pkl", dataset_size=1000, test_size=0.01)
     x_train, _, y_train, _ = formatter.generate_train_test_split()
-    root = "/share-files/models/"
-    paths = ["mlp_best_model.joblib", "logistic regression_best_model.joblib", "xgboost_best_model.joblib", "random forest_best_model.joblib"]
-    for path in paths:
+
+    for path in paths[:1]:
         print(f"loading model: {path}")
-        model = joblib.load(root+path)
+        model = joblib.load(path)
         # model_members(model)
         calculate_feature_importance(model, x_train, y_train)
 
