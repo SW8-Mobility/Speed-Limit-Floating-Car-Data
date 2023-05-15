02177d35fc12fc5a15f7febca4bdc6f76639cbf6
AlexanderHHansen <61501072+AlexanderHHansen@users.noreply.github.com>
2023-05-15 09:46:24 +0200

    change

--- data_exploration/metrics_to_latex.py
+++ data_exploration/metrics_to_latex.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 
-metrics_df = pd.read_csv("metric.csv")
+metrics_df = pd.read_csv("metric.txt")
 metrics_df['model'] = metrics_df['model'].apply(lambda model_name: model_name.replace('_', ' '))
 metrics_cols = metrics_df.columns.drop(['model'])
 for col in metrics_cols:
