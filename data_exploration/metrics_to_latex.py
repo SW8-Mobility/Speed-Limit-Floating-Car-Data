094006d2eac43bd985503f7845d870db04a48ced
Lint Action <lint-action@samuelmeuli.com>
2023-05-15 07:46:57 +0000

    Fix code style issues with Black

--- data_exploration/metrics_to_latex.py
+++ data_exploration/metrics_to_latex.py
@@ -1,8 +1,10 @@
-import pandas as pd 
+import pandas as pd
 
 metrics_df = pd.read_csv("metric.csv")
-metrics_df['model'] = metrics_df['model'].apply(lambda model_name: model_name.replace('_', ' '))
-metrics_cols = metrics_df.columns.drop(['model'])
+metrics_df["model"] = metrics_df["model"].apply(
+    lambda model_name: model_name.replace("_", " ")
+)
+metrics_cols = metrics_df.columns.drop(["model"])
 for col in metrics_cols:
     metrics_df[col] = metrics_df[col].apply(lambda metric: str(round(float(metric), 2)))
 metrics_df.to_latex("metrics_latex.txt", index=False)
