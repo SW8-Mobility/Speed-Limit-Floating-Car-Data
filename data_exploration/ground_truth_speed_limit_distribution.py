b2798a31ee25a5c3c7beb4cd850cf913446dec81
CasperKjaerhus <casperkjaerhusleth@gmail.com>
2023-05-16 07:41:36 +0000

    added comments

--- data_exploration/ground_truth_speed_limit_distribution.py
+++ data_exploration/ground_truth_speed_limit_distribution.py
@@ -6,6 +6,8 @@ share_files = "/share-files/raw_data_pkl/"
 path = "features_and_ground_truth_combined.pkl"
 
 df = pd.read_pickle(share_files + path)
+
+# Get distribution and total amount of occurrences
 speed_dist_df = df[Feature.HAST_GAELDENDE_HAST.value].value_counts(normalize=True) * 100
 speed_amount_df = df[Feature.HAST_GAELDENDE_HAST.value].value_counts()
 
