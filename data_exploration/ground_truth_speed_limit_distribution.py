15e0031e1b0165c2ea3c4d27ab3d80155819ab57
CasperKjaerhus <casperkjaerhusleth@gmail.com>
2023-05-16 07:20:17 +0000

    get amount total also

--- data_exploration/ground_truth_speed_limit_distribution.py
+++ data_exploration/ground_truth_speed_limit_distribution.py
@@ -6,6 +6,9 @@ share_files = "/share-files/raw_data_pkl/"
 path = "features_and_ground_truth_combined.pkl"
 
 df = pd.read_pickle(share_files + path)
-road_dist_df = df[Feature.HAST_GAELDENDE_HAST.value].value_counts(normalize=True) * 100
-
-print(road_dist_df)
+speed_dist_df = df[Feature.HAST_GAELDENDE_HAST.value].value_counts(normalize=True) * 100
+speed_amount_df = df[Feature.HAST_GAELDENDE_HAST.value].value_counts()
+print("---- SPEED DISTRIBUTION ----")
+print(speed_dist_df)
+print("---- SPEED AMOUNT ----")
+print(speed_amount_df)
