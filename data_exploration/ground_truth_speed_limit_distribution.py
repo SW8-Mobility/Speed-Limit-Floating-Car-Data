5d767b5a128e49257307490e80c191885ff60399
CasperKjaerhus <casperkjaerhusleth@gmail.com>
2023-05-16 07:29:03 +0000

    remove print headers

--- data_exploration/ground_truth_speed_limit_distribution.py
+++ data_exploration/ground_truth_speed_limit_distribution.py
@@ -8,7 +8,6 @@ path = "features_and_ground_truth_combined.pkl"
 df = pd.read_pickle(share_files + path)
 speed_dist_df = df[Feature.HAST_GAELDENDE_HAST.value].value_counts(normalize=True) * 100
 speed_amount_df = df[Feature.HAST_GAELDENDE_HAST.value].value_counts()
-print("---- SPEED DISTRIBUTION ----")
+
 print(speed_dist_df)
-print("---- SPEED AMOUNT ----")
 print(speed_amount_df)
